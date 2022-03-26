6213e880460d6847715a589c4738a76bbc32705d
ydk1204 <ejrbdi@naver.com>
2022-03-26 15:53:59 +0800

    STYLE : 설명 문구 애니메이션 효과

--- src/components/addMent/AddMent.jsx
+++ src/components/addMent/AddMent.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./AddMent.module.css";
 
 const AddMent = (props) => {
+  useEffect(() => {
+    const rootLine = document.querySelector(".Mentbox");
+    const headerText = document.querySelector(".headerText");
+
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          headerText.classList.add("anim");
+        }
+      });
+    }, options);
+
+    observer.observe(rootLine);
+
+    return;
+  }, []);
+
+  useEffect(() => {
+    const rootLine = document.querySelector(".headerText");
+    const explanText = document.querySelector(".explan");
+
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          explanText.classList.add("delayAnim");
+        }
+      });
+    }, options);
+
+    observer.observe(rootLine);
+
+    return;
+  }, []);
   return (
     <>
       <div className={styles.container}>
@@ -23,7 +68,26 @@ const AddMent = (props) => {
             </div>
           </div>
         </div>
-        <div className={styles.rightBox}>asasda</div>
+        <div className={`${styles.rightBox} Mentbox`}>
+          <div className={`${styles.headerFlexRow} headerText`}>
+            게임 개발 때{" "}
+            <span className={styles.textPointColor}>배운 기술</span>
+          </div>
+          <div className="explan">
+            C언어와 C#을 배우고, 유니티 엔진과
+            <br />
+            언리얼 엔진을 통해{" "}
+            <span className={styles.gamePointColor}>게임 개발</span>을 했습니다.
+            <br />
+            게임 기획, 개발등 지금까지 학습한 기술이
+            <br />
+            아깝긴 했지만, 내가 지금까지 사용했던
+            <br />
+            웹의 구조를 보고 비슷한 것을 만들 수 있다는
+            <br />
+            기대가 <span className={styles.semiPointColor}>저를 이끌었죠.</span>
+          </div>
+        </div>
       </div>
     </>
   );
